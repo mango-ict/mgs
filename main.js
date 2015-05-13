eadcab9c41acf4f053efa33e46637c0cee2c91e8
MangoICT <service@mangoict.com>
2015-05-13 06:43:54 +0200

    Started adding support for command line argument "init" to initialise a mango website project.

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 #! /usr/local/bin/node
 
+// Get the util
+var util = require("util");
+
+// Output CLI
 console.log('Mango ICT - CLI');
+
+// Done with CLI
+function done(msg){
+  console.log(msg);
+  process.exit();
+}
+	
+// Init Project
+function initProject () {
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  process.stdin.on('data', function (text) {
+    console.log('received data:', util.inspect(text));
+    if (text === 'quit\n') {
+      done('The project has been created');
+    }
+  });
+}
+
+// Process commandline arguments
+process.argv.forEach(function (val, index, array) {
+	switch (val) {
+		case "--version":
+			console.log("current version is 1.0.2"); 
+			break;
+		case "init":
+			initProject();
+			break;
+	}
+});
